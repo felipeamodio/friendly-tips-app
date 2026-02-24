8bd5bdf7366c177cd2e87b0c9059d93ff1e3cbec
Felipe Alves Amodio <felipeaamodio@gmail.com>
2026-02-24 19:31:14 -0300

    Initial Commit

---
+++ Friendly Tips/Friendly_TipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Friendly_TipsApp.swift
+//  Friendly Tips
+//
+//  Created by Felipe Alves Amodio on 24/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Friendly_TipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
